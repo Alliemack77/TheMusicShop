4570b682c0e18c4a2f36c5da5cf6079b53537031
Allison Leggett <alliemack77@yahoo.com>
2021-06-21 17:24:23 +0200

    changed width on cards

--- dist/fontawesome.bd9656ef.js
+++ dist/fontawesome.bd9656ef.js
@@ -2662,7 +2662,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55077" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63922" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
